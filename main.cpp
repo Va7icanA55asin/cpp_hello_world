b9daa94284d22af8fd07ded3bcf697648b97c0bb
GCBlandin <colebland000@yahoo.com>
2023-02-10 22:54:20 -0600

    Added A couple More Tests

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <limits>
 #include <iostream>
+#include <random>
 using namespace std;
 
 void basic_hello_world();
@@ -13,6 +14,8 @@ void looping_hello_world();
 void looping_hello_world_left_aligned();
 void input_test();
 void input_error_handling();
+void number_output();
+void guessing_game();
 
 int main() {
     cout << "Starting Main" << endl;
@@ -24,6 +27,10 @@ int main() {
     looping_hello_world_left_aligned();
     cout << "*****************************************" << endl;
     input_test();
+    cout << "*****************************************" << endl;
+    number_output();
+    cout << "*****************************************" << endl;
+    guessing_game();
     cout << "*****************************************" << endl;
     cout << "Done with Main" << endl;
     return 0;
@@ -58,6 +65,7 @@ void looping_hello_world_left_aligned() {
 }
 
 void input_test() {
+    cout << "Input Test" << endl;
     int input_var = 0;
     // Enter the do while loop and stay there until either
     // a non-numeric is entered, or -1 is entered. Note that
@@ -87,4 +95,42 @@ void input_error_handling() {
     cout << "You entered a non-numeric. Please only use numbers. Continuing..." << endl;
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+void number_output() {
+    cout << "Number Output" << endl;
+    cout << " 1\t2\t3\t4\t5\t6\t7\t8\t9" << endl << "" << endl;
+    for (int c = 1; c < 10; c++) {
+        cout << c << "| ";
+        for (int i = 1; i < 10; i++) {
+            cout << i * c << '\t';
+        }
+        cout << endl;
+    }
+}
+
+void guessing_game() {
+    cout << "Welcome to the guessing game" << endl;
+
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution<int> dist(1, 100);
+    int random_number = dist(mt);
+    int guess = -1;
+
+    cout << "Guess the number 1-100 (0 to give up): " << endl;
+    do {
+        if (!(cin >> guess)) {
+            input_error_handling();
+            guess = -1;
+        } else if (guess != 0) {
+            if (guess < random_number) {
+                cout << guess << " is too low, try again" << endl;
+            } else if (guess > random_number) {
+                cout << guess << " is too high, try again" << endl;
+            } else {
+                cout << "You got it!" << endl;
+            }
+        }
+    } while (guess != 0 && guess != random_number);
 }
