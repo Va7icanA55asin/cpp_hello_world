ec6c3f793d7bb3f90fdf709959f6bfedc1e502ed
GCBlandin <colebland000@yahoo.com>
2023-02-17 17:16:45 -0600

    Added Second Main Added another main file based on the C++ tutorial

---
+++ main_2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Gabe Blandin on 2/17/2023.
+// This is the secondary main entry point for this hello_world project
+// This is based on the docs at https://cplusplus.com/doc/tutorial/
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello" << endl;
+    return 0;
+}
